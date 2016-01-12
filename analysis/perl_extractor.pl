21308e45f9ee048919ac1e7838c773455fff08d6
Luka Stanisic <luka.stanisic@imag.fr>
2016-01-12 16:47:13 +0100

    Easier if multiple runs are stored in a same file

---
+++ analysis/perl_extractor.pl
@@ -0,0 +1,17 @@
+use strict;
+
+my($line);
+my($size);
+
+print "Size, Type, Time\n" ;
+while($line=<>) {
+    chomp $line;
+    if($line =~/^Array size: ([\d\.]*)$/) {
+        $size = $1;
+        next;
+    } 
+    if($line =~/^(.*) quicksort.*: ([\d\.]*) sec.$/) {
+        print "$size, \"$1\", $2\n" ;
+        next;
+    } 
+}
